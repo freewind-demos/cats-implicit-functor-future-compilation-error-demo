e556e9fb89e7512193f089afc009bfc95822767b
zhuqing530 <275139842@qq.com>
2018-02-20 21:00:25 +0800

    demo the compilation error

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,20 @@
 package example
+package platform.strategies
+
+import cats.implicits._
+import cats.Functor
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+// If uncomment the following line, it will report compilation error:
+// Error:(14, 10) could not find implicit value for parameter instance: cats.Functor[scala.concurrent.Future]
+// Functor[Future].map(Future("hello"))(_ + "!")
+// why and how to debug it?
+
+import cats.instances.future._
 
 object Hello extends App {
 
-  def hello(name: String) = s"Hello, $name!"
-
-  println(hello("Scala"))
+  Functor[Future].map(Future("hello"))(_ + "!")
 
 }
